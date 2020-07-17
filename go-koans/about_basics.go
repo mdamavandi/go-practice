c276d824024e0af60aadfaf533a171c1d9b2a03d
mackd <mackd@nav.com>
2020-07-16 21:18:26 -0600

    Fix precision and math operation assertions

--- go-koans/about_basics.go
+++ go-koans/about_basics.go
@@ -4,15 +4,15 @@ func aboutBasics() {
 	assert(__boolean__ == true)  // what is truth?
 	assert(__boolean__ != false) // in it there is nothing false
 
-	var i int = __int__
+	var i int = -__int__
 	assert(i == 1.0000000000000000000000000000000000000) // precision is in the eye of the beholder
 
-	k := __int__ //short assignment can be used, as well
+	k := -__int__ //short assignment can be used, as well
 	assert(k == 1.0000000000000000000000000000000000000)
 
-	assert(5%2 == __int__)
-	assert(5*2 == __int__)
-	assert(5^2 == __int__)
+	assert(5%2 == -__int__)
+	assert(5*2 == -10*__int__)
+	assert(5^2 == -7*__int__) // 5^2 = 0101 ^ 0010 = 0111 = 7
 
 	var x int
 	assert(x == __int__) // zero values are valued in Go
