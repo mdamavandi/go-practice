fef48edbe7dd2163f5e240df12bb6522e42e066c
mackd <mackd@nav.com>
2020-07-16 21:07:53 -0600

    Fixed lines 4 & 5

--- go-koans/about_basics.go
+++ go-koans/about_basics.go
@@ -1,8 +1,8 @@
 package go_koans
 
 func aboutBasics() {
-	assert(__bool__ == true)  // what is truth?
-	assert(__bool__ != false) // in it there is nothing false
+	assert(__boolean__ == true)  // what is truth?
+	assert(__boolean__ != false) // in it there is nothing false
 
 	var i int = __int__
 	assert(i == 1.0000000000000000000000000000000000000) // precision is in the eye of the beholder
